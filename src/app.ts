0bce1a8b97e4aa2ec401e4351c871fb99014d09a
Vincent Fillon <v.fillon.pro@gmail.com>
2019-03-10 19:52:16 +0100

    Ajout du bouton clear cache

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,18 @@ if ('serviceWorker' in navigator) {
 }
 
 window.addEventListener('load', () => {
+  const clearBtn: HTMLElement = document.getElementById('clear-cache-btn');
+
+  clearBtn.addEventListener('click', e => {
+    if ('caches' in window) {
+      caches.keys().then(keys => {
+        keys.forEach(key => {
+          caches.delete(key);
+        });
+      });
+    }
+  });
+
   const status: HTMLElement = document.getElementById('status-indicator');
 
   function updateOnlineStatus() {
@@ -37,88 +49,88 @@ window.addEventListener('load', () => {
 
   window.addEventListener('online', updateOnlineStatus);
   window.addEventListener('offline', updateOnlineStatus);
-});
 
-const mainRenderer: Renderer = new Renderer('#sw-demo-container');
+  const mainRenderer: Renderer = new Renderer('#sw-demo-container');
 
-const imgGrid: HtmlElementI = {
-  tagName: 'div',
-  classes: 'row align-items-end',
-  children: []
-};
-
-images.forEach((image: ImageI, index: number) => {
-  imgGrid.children[index] = {
+  const imgGrid: HtmlElementI = {
     tagName: 'div',
-    classes: 'col',
-    children: [
-      {
-        tagName: 'figure',
-        id: 'figure-' + index,
-        classes: 'text-center',
-        children: [
-          // dog.htmlStruct,
-          {
-            tagName: 'img',
-            attributes: [
-              { name: 'src', value: './assets/default_dog.jpg' },
-              { name: 'alt', value: 'Default dog' },
-              { name: 'style', value: 'max-width: 300px; max-height: 300px' }
-            ]
-          },
-          {
-            tagName: 'caption',
-            classes: 'text-center',
-            children: [{ tagName: 'span', children: [{ tagName: 'strong', innerHtml: 'Default dog' }] }]
-          }
-        ]
-      }
-    ]
+    classes: 'row align-items-end',
+    children: []
   };
 
-  if (navigator.onLine) {
-    fetch(image.url, {
-      headers: {
-        Authorization: 'Client-ID 2023f60e723bc951d0a13fcf586156faaa7e1ec9f07228e5156fb5986456ee3e'
-      }
-    })
-      .then(response => {
-        if (response.ok) {
-          return response.blob();
-        } else {
-          console.error(new Error(response.statusText));
+  images.forEach((image: ImageI, index: number) => {
+    imgGrid.children[index] = {
+      tagName: 'div',
+      classes: 'col',
+      children: [
+        {
+          tagName: 'figure',
+          id: 'figure-' + index,
+          classes: 'text-center',
+          children: [
+            // dog.htmlStruct,
+            {
+              tagName: 'img',
+              attributes: [
+                { name: 'src', value: './assets/default_dog.jpg' },
+                { name: 'alt', value: 'Default dog' },
+                { name: 'style', value: 'max-width: 300px; max-height: 300px' }
+              ]
+            },
+            {
+              tagName: 'caption',
+              classes: 'text-center',
+              children: [{ tagName: 'span', children: [{ tagName: 'strong', innerHtml: 'Default dog' }] }]
+            }
+          ]
+        }
+      ]
+    };
+
+    if (navigator.onLine) {
+      fetch(image.url, {
+        headers: {
+          Authorization: 'Client-ID 2023f60e723bc951d0a13fcf586156faaa7e1ec9f07228e5156fb5986456ee3e'
         }
       })
-      .then(blob => {
-        let imageURL: string = window.URL.createObjectURL(blob);
-        imgGrid.children[index].children[0].children = [
-          {
-            tagName: 'img',
-            attributes: [
-              { name: 'src', value: imageURL },
-              { name: 'alt', value: image.alt },
-              { name: 'style', value: 'max-width: 300px; max-height: 300px' }
-            ]
-          },
-          {
-            tagName: 'caption',
-            classes: 'text-center',
-            children: [
-              { tagName: 'span', children: [{ tagName: 'strong', innerHtml: image.name }] },
-              { tagName: 'br' },
-              {
-                tagName: 'a',
-                attributes: [{ name: 'href', value: image.creditsUrl }, { name: 'target', value: '_blank' }],
-                innerHtml: image.credits
-              }
-            ]
+        .then(response => {
+          if (response.ok) {
+            return response.blob();
+          } else {
+            console.error(new Error(response.statusText));
           }
-        ];
-        mainRenderer.clearContent();
-        mainRenderer.appendChild(mainRenderer.renderEl(imgGrid));
-      });
-  }
-});
+        })
+        .then(blob => {
+          let imageURL: string = window.URL.createObjectURL(blob);
+          imgGrid.children[index].children[0].children = [
+            {
+              tagName: 'img',
+              attributes: [
+                { name: 'src', value: imageURL },
+                { name: 'alt', value: image.alt },
+                { name: 'style', value: 'max-width: 300px; max-height: 300px' }
+              ]
+            },
+            {
+              tagName: 'caption',
+              classes: 'text-center',
+              children: [
+                { tagName: 'span', children: [{ tagName: 'strong', innerHtml: image.name }] },
+                { tagName: 'br' },
+                {
+                  tagName: 'a',
+                  attributes: [{ name: 'href', value: image.creditsUrl }, { name: 'target', value: '_blank' }],
+                  innerHtml: image.credits
+                }
+              ]
+            }
+          ];
+          mainRenderer.clearContent();
+          mainRenderer.appendChild(mainRenderer.renderEl(imgGrid));
+        });
+    }
+  });
 
-mainRenderer.clearContent();
-mainRenderer.appendChild(mainRenderer.renderEl(imgGrid));
+  mainRenderer.clearContent();
+  mainRenderer.appendChild(mainRenderer.renderEl(imgGrid));
+});
